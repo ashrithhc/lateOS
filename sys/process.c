dd454215594a604389b8080075e16abfe6ba5f5d
ashrithhc <ashrithhcs@gmail.com>
2018-02-05 17:05:20 -0500

    fix

--- sys/process.c
+++ sys/process.c
@@ -72,15 +72,13 @@ void ps()
 	}
 }
 
-int initTaskVariables(task_struct *task, char *filename){
-    int pid = newPID();
+void initTaskVariables(task_struct *task, char *filename, int pid){
     strcpy(task->name, filename);
     strcpy(task->curr_dir, "/");
     task->ppid = 0;
     task->pid = pid;
     task->vm = NULL;
     task->state = RUNNING;
-    return pid;
 }
 
 void create_process(char* filename){
@@ -92,8 +90,9 @@ void create_process(char* filename){
 	}
     char a[50];
     strcpy(a,filename);
+	int pid = newPID();
 	task_struct* ts = (task_struct *) &taskQueue[pid];
-    int pid = initTaskVariables(ts, filename);
+    initTaskVariables(ts, filename, pid);
 	Elf64_Ehdr* eh = (Elf64_Ehdr*)(f_a); //512 - to offset tar info
 	int no_ph = eh->e_phnum;
 	uint64_t* pml4 = (uint64_t *)getNewPage();
