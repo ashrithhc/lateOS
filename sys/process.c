381805c8a08dc04e16bde6298b384983f455e94e
ashrithhc <ashrithhcs@gmail.com>
2018-04-22 05:40:35 -0400

    fixing gamma function

--- sys/process.c
+++ sys/process.c
@@ -133,6 +133,49 @@ uint64_t* yappaFunction(uint64_t fileAddress, task_struct* ts){
     return pml4;
 }
 
+uint64_t* gammaFunction(uint64_t fileAddress, task_struct* ts){
+    Elf64_Ehdr* eh = (Elf64_Ehdr*)(fileAddress);
+    ts->regs.rip = eh->e_entry;
+    uint64_t* pml4 = (uint64_t *)(ts->pml4e + kernbase);
+    dealloc_pml4((ts->pml4e));
+
+    for(int i=eh->e_phnum;i>0;i--){
+        Elf64_Phdr* ep = (Elf64_Phdr*)(fileAddress + (eh->e_phoff));
+        ep = ep + (i-1);
+        if(ep->p_type == 1){               
+
+            vma* vm = (vma *)kmalloc(sizeof(struct vmaStruct));
+            vm->beginAddress = ep->p_vaddr;
+            vm->lastAddress = ep->p_vaddr+ep->p_memsz;
+            uint64_t k = vm->beginAddress;
+            if((((uint64_t)(ep->p_vaddr))% ((uint64_t)pageSize)) != 0){
+                k = (uint64_t)((uint64_t)ep->p_vaddr & (uint64_t)0xFFFFFFFFFFFFF000);
+            }
+            if(ts->vm == NULL){
+                vm->next = NULL;
+                ts->vm = vm;
+            }
+            else{
+                vm->next = ts->vm;
+                ts->vm = vm;
+            }
+            for(;k<( vm->lastAddress);k+=pageSize){
+                uint64_t yy = getFreeFrame();
+                init_pages_for_process(k,yy, pml4);
+            }
+
+            uint64_t pcr3;
+            __asm__ __volatile__ ("movq %%cr3,%0;" :"=r"(pcr3)::);
+            uint64_t* pl =( uint64_t* )((uint64_t)pml4 - (uint64_t)kernbase);
+            __asm__ volatile ("movq %0, %%cr3;" :: "r"(pl));
+            memcpy((void*)vm->beginAddress,(void*)(eh + ep->p_offset), (uint64_t)(ep->p_filesz));
+            memset((void*)(vm->beginAddress + (uint64_t)(ep->p_filesz)), 0, (uint64_t)(ep->p_memsz) - (uint64_t)(ep->p_filesz));
+            __asm__ volatile ("movq %0, %%cr3;" :: "r"(pcr3));
+        }
+    }
+    return pml4;
+}
+
 void setMyVMA(task_struct *ts, uint64_t from, uint64_t toend){
     vma* taskVMA = (vma *) kmalloc(sizeof(struct vmaStruct));
     taskVMA->beginAddress = from;
@@ -330,53 +373,6 @@ int execvpe(char* path, char *argv[],char* env[]){
             argc++;
         }
     }
-    /*if(validPath(path)){
-        strcpy(file, &(r->curr_dir[1]));
-        strcat(file, path+2);
-        uint64_t y = get_file_address(file)+512;
-        if(y<512){
-            return -1;
-        }
-        if( *((char *)y) == '!' && *((char *)y+1) == '#') {
-            char ex[50];
-            int q= 0;
-            y+=2;
-            while( (*(char *) y) != '\n'){
-                if( (*(char *) y) != ' ') {
-                    ex[q++] = *(char *) y;
-                }
-                y = y+1;
-            }
-            ex[q++]='\0';
-            if(strlen(ex) == 0){
-                strcpy(ex,"bin/sbush");
-            }
-            fileAddress = get_file_address(ex) + 512;
-            strcpy(args[argc++], argv[0]);
-            strcpy(args[argc++], path+2);
-        }
-        else {
-            fileAddress = get_file_address("bin/sbush") + 512;
-            strcpy(args[argc++], argv[0]);
-            strcpy(args[argc++], path+2);
-        }
-    }
-    else {
-        strcpy(file, "bin/");
-        strcat(file, path);
-        strcpy(ts->name, file);
-
-
-        fileAddress = get_file_address(file) + 512;
-        if (fileAddress < 512) {
-            return -1;
-        }
-
-        while (argv[argc]) {
-            strcpy(args[argc], argv[argc]);
-            argc++;
-        }
-    }*/
     while (env[envl]) {
         strcpy(envs[envl], env[envl]);
         argc++;
@@ -384,7 +380,7 @@ int execvpe(char* path, char *argv[],char* env[]){
     if (fileAddress < 512) {
         return -1;
     }
-	Elf64_Ehdr* eh = (Elf64_Ehdr*)(fileAddress);
+/*	Elf64_Ehdr* eh = (Elf64_Ehdr*)(fileAddress);
 	ts->regs.rip = eh->e_entry;
 	uint64_t* pml4 = (uint64_t *)(ts->pml4e + kernbase);
     dealloc_pml4((ts->pml4e));
@@ -422,7 +418,8 @@ int execvpe(char* path, char *argv[],char* env[]){
 			memset((void*)(vm->beginAddress + (uint64_t)(ep->p_filesz)), 0, (uint64_t)(ep->p_memsz) - (uint64_t)(ep->p_filesz));
 			__asm__ volatile ("movq %0, %%cr3;" :: "r"(pcr3));
 		}
-	}
+	}*/
+    uint64_t pml4 = gammaFunction(fileAddress, ts);
 
     vma* vm2 = (vma *)kmalloc(sizeof(struct vmaStruct));
     vm2->beginAddress = 0x4B0FFFFF0000;
