0a0377891c2f9f73130916cc8a899f64abedcf76
ashrithhc <ashrithhcs@gmail.com>
2018-04-24 20:40:04 -0400

    code clean - gamma fix

--- sys/process.c
+++ sys/process.c
@@ -151,25 +151,23 @@ uint64_t* gammaFunction(uint64_t fileAddress, taskStruct *ts){
     for(int i=eh->e_phnum;i>0;i--){
         Elf64_Phdr* ep = (Elf64_Phdr*)(fileAddress + (eh->e_phoff));
         ep = ep + (i-1);
-        if(ep->p_type == 1){               
-
-            vmaStruct* vm = (vmaStruct *)kmalloc(sizeof(struct vmaStruct));
-            uint64_t k = setupVMA(vm, ep); 
-            vm->next = validateTaskVM(ts);
-            ts->vm = vm;
-            for(;k<( vm->lastAddress);k+=pageSize){
-                uint64_t yy = getFreeFrame();
-                init_pages_for_process(k,yy, pml4);
-            }
-
-            uint64_t pcr3;
-            __asm__ __volatile__ ("movq %%cr3,%0;" :"=r"(pcr3)::);
-            uint64_t* pl =( uint64_t* )((uint64_t)pml4 - (uint64_t)kernbase);
-            __asm__ volatile ("movq %0, %%cr3;" :: "r"(pl));
-            memcpy((void*)vm->beginAddress,(void*)(eh + ep->p_offset), (uint64_t)(ep->p_filesz));
-            memset((void*)(vm->beginAddress + (uint64_t)(ep->p_filesz)), 0, (uint64_t)(ep->p_memsz) - (uint64_t)(ep->p_filesz));
-            __asm__ volatile ("movq %0, %%cr3;" :: "r"(pcr3));
+        if(ep->p_type != 1) continue;
+        vmaStruct* vm = (vmaStruct *)kmalloc(sizeof(struct vmaStruct));
+        uint64_t k = setupVMA(vm, ep); 
+        vm->next = validateTaskVM(ts);
+        ts->vm = vm;
+        for(;k<( vm->lastAddress);k+=pageSize){
+            uint64_t yy = getFreeFrame();
+            init_pages_for_process(k,yy, pml4);
         }
+
+        uint64_t pcr3;
+        __asm__ __volatile__ ("movq %%cr3,%0;" :"=r"(pcr3)::);
+        uint64_t* pl =( uint64_t* )((uint64_t)pml4 - (uint64_t)kernbase);
+        __asm__ volatile ("movq %0, %%cr3;" :: "r"(pl));
+        memcpy((void*)vm->beginAddress,(void*)(eh + ep->p_offset), (uint64_t)(ep->p_filesz));
+        memset((void*)(vm->beginAddress + (uint64_t)(ep->p_filesz)), 0, (uint64_t)(ep->p_memsz) - (uint64_t)(ep->p_filesz));
+        __asm__ volatile ("movq %0, %%cr3;" :: "r"(pcr3));
     }
     return pml4;
 }
@@ -217,8 +215,6 @@ void setTaskRSP(char *name, taskStruct *ts){
         else if (i==2) *(ts->rsp) = (uint64_t)((ts->rsp)+1);
         else *(ts->rsp) = 0x1;
     }
-
-
     currentTask = ts;
 }
 
