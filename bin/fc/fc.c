0d9ac677e3bf0e17c6cfda6407560710a1b518ab
ashrithhc <ashrithhcs@gmail.com>
2018-04-27 12:58:35 -0400

    removing fc

--- bin/fc/fc.c
+++ bin/fc/fc.c
@@ -1,4 +1,4 @@
-#include <sys/defs.h>
+/*#include <sys/defs.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
@@ -25,3 +25,4 @@ int main(int argc, char *argv[], char* envp[])
         }
     }
 }
+*/
