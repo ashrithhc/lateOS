902177c61f6abc2913b5db79cc7724d82d005cac
ashrithhc <ashrithhcs@gmail.com>
2018-04-30 02:24:28 -0400

    wait syscall

--- libc/wait.c
+++ libc/wait.c
@@ -4,6 +4,9 @@
 typedef struct rusage r;
 pid_t wait(int* status){
 	_syscall4(pid_t,wait4,pid_t,-1,int*,status,int,0,rusage,NULL);
+	long retVal;
+	__asm__ volatile ("movq %1, %%rax; movq %2, %%rbx; movq %3, %%rcx; movq %4, %%rdx; movq %5, %%rsi; int $0x80; movq %%rax, %0" : "=m" (retVal) : "g" (61),"g" ((long)(-1)),"g" ((long)(status)), "g" ((long)(0)), "g" ((long)(NULL)) : "rax", "rbx", "rcx", "rdx", "rsi");
+	return (pid_t)(retVal);
 }
 pid_t waitpid(pid_t pid,pid_t *status){
 	// _syscall2(pid_t,waitid,int,pid,int*,status);
