ee149d73fa30026bf76aefef83df1cc4236817d4
ashrithhc <ashrithhcs@gmail.com>
2018-04-30 02:42:18 -0400

    removing syscalls

--- include/syscall.h
+++ include/syscall.h
@@ -2,19 +2,5 @@
 #define _SYSCALL_H
 #include "syscall_const.h"
 
-#define _sys_return(type,res) \
-do{ 				\
-	return (type)(res); \
-}while(0) 
-
-#define _syscall(type,name) \
-do{ \
-long __res; \
-__asm__ volatile (  "int $0x80;" \
-                  : "=a" (__res) \
-                  : "a"(__NR_##name) \
-                  :); \
- return (type) (__res); \
-}while(0)
 
 #endif
