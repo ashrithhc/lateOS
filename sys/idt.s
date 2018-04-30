97dec0ad4193597f58ec45279576b613da1b48a2
ashrithhc <ashrithhcs@gmail.com>
2018-04-30 14:40:32 -0400

    removing code from idt.s

--- sys/idt.s
+++ sys/idt.s
@@ -122,163 +122,163 @@ isr_0:
 	callq isr0
 	pop
 	iretq
-isr_1:
-	push
-	callq isr1
-	pop
-	iretq
-isr_2:
-	push
-	callq isr2
-	pop
-	iretq
-isr_3:
-	push
-	callq isr3
-	pop
-	iretq
-isr_4:
-	push
-	callq isr4
-	pop
-	iretq
-isr_5:
-	push
-	callq isr5
-	pop
-	iretq
-isr_6:
-	push
-	callq isr6
-	pop
-	iretq
-isr_7:
-	push
-	callq isr7
-	pop
-	iretq
-isr_8:
-	push
-	callq isr8
-	pop
-	iretq
-isr_9:
-	push
-	callq isr9
-	pop
-	iretq
-isr_10:
-	push
-	callq isr10
-	pop
-	iretq
-isr_11:
-	push
-	callq isr11
-	pop
-	iretq
-isr_12:
-	push
-	callq isr12
-	pop
-	iretq
-isr_13:
-	push
-	callq isr13
-	pop
-	iretq
+# isr_1:
+# 	push
+# 	callq isr1
+# 	pop
+# 	iretq
+# isr_2:
+# 	push
+# 	callq isr2
+# 	pop
+# 	iretq
+# isr_3:
+# 	push
+# 	callq isr3
+# 	pop
+# 	iretq
+# isr_4:
+# 	push
+# 	callq isr4
+# 	pop
+# 	iretq
+# isr_5:
+# 	push
+# 	callq isr5
+# 	pop
+# 	iretq
+# isr_6:
+# 	push
+# 	callq isr6
+# 	pop
+# 	iretq
+# isr_7:
+# 	push
+# 	callq isr7
+# 	pop
+# 	iretq
+# isr_8:
+# 	push
+# 	callq isr8
+# 	pop
+# 	iretq
+# isr_9:
+# 	push
+# 	callq isr9
+# 	pop
+# 	iretq
+# isr_10:
+# 	push
+# 	callq isr10
+# 	pop
+# 	iretq
+# isr_11:
+# 	push
+# 	callq isr11
+# 	pop
+# 	iretq
+# isr_12:
+# 	push
+# 	callq isr12
+# 	pop
+# 	iretq
+# isr_13:
+# 	push
+# 	callq isr13
+# 	pop
+# 	iretq
 isr_14:
 	push
 	callq isr14
 	pop
 	add $8, %rsp
 	iretq
-isr_15:
-	push
-	callq isr15
-	pop
-	iretq
-isr_16:
-	push
-	callq isr16
-	pop
-	iretq
-isr_17:
-	push
-	callq isr17
-	pop
-	iretq
-isr_18:
-	push
-	callq isr18
-	pop
-	iretq
-isr_19:
-	push
-	callq isr19
-	pop
-	iretq
-isr_20:
-	push
-	callq isr20
-	pop
-	iretq
-isr_21:
-	push
-	callq isr21
-	pop
-	iretq
-isr_22:
-	push
-	callq isr22
-	pop
-	iretq
-isr_23:
-	push
-	callq isr23
-	pop
-	iretq
-isr_24:
-	push
-	callq isr24
-	pop
-	iretq
-isr_25:
-	push
-	callq isr25
-	pop
-	iretq
-isr_26:
-	push
-	callq isr26
-	pop
-	iretq
-isr_27:
-	push
-	callq isr27
-	pop
-	iretq
-isr_28:
-	push
-	callq isr28
-	pop
-	iretq
-isr_29:
-	push
-	callq isr29
-	pop
-	iretq
+# isr_15:
+# 	push
+# 	callq isr15
+# 	pop
+# 	iretq
+# isr_16:
+# 	push
+# 	callq isr16
+# 	pop
+# 	iretq
+# isr_17:
+# 	push
+# 	callq isr17
+# 	pop
+# 	iretq
+# isr_18:
+# 	push
+# 	callq isr18
+# 	pop
+# 	iretq
+# isr_19:
+# 	push
+# 	callq isr19
+# 	pop
+# 	iretq
+# isr_20:
+# 	push
+# 	callq isr20
+# 	pop
+# 	iretq
+# isr_21:
+# 	push
+# 	callq isr21
+# 	pop
+# 	iretq
+# isr_22:
+# 	push
+# 	callq isr22
+# 	pop
+# 	iretq
+# isr_23:
+# 	push
+# 	callq isr23
+# 	pop
+# 	iretq
+# isr_24:
+# 	push
+# 	callq isr24
+# 	pop
+# 	iretq
+# isr_25:
+# 	push
+# 	callq isr25
+# 	pop
+# 	iretq
+# isr_26:
+# 	push
+# 	callq isr26
+# 	pop
+# 	iretq
+# isr_27:
+# 	push
+# 	callq isr27
+# 	pop
+# 	iretq
+# isr_28:
+# 	push
+# 	callq isr28
+# 	pop
+# 	iretq
+# isr_29:
+# 	push
+# 	callq isr29
+# 	pop
+# 	iretq
 
-isr_30:
-	push
-	callq isr30
-	pop
-	iretq
-isr_31:
-	push
-	callq isr31
-	pop
-	iretq
+# isr_30:
+# 	push
+# 	callq isr30
+# 	pop
+# 	iretq
+# isr_31:
+# 	push
+# 	callq isr31
+# 	pop
+# 	iretq
 isr_128:
 	cli
 	pushq %rbx
