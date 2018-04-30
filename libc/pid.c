0df4451d10a6ecb73665250176adb91c91f9df26
ashrithhc <ashrithhcs@gmail.com>
2018-04-30 02:34:04 -0400

    fix

--- libc/pid.c
+++ libc/pid.c
@@ -6,10 +6,12 @@ pid_t getpid_call(){
     // _syscall(pid_t, getTaskPID);
     long retVal;
 	__asm__ volatile ("int $0x80;" : "=a"(retVal) : "a"(39) : );
+	return (pid_t)retVal;
 }
 
 pid_t getppid_call(){
     // _syscall(pid_t, getTaskPPID);
     long retVal;
 	__asm__ volatile ("int $0x80;" : "=a"(retVal) : "a"(110) : );
+	return (pid_t)retVal;
 }
