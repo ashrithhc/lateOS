41f37286a136e47357efefd4d965a0c515008de7
ashrithhc <ashrithhcs@gmail.com>
2018-04-30 02:18:07 -0400

    removing syscall3

--- include/syscall.h
+++ include/syscall.h
@@ -17,16 +17,6 @@ __asm__ volatile (  "int $0x80;" \
  return (type) (__res); \
 }while(0)
 
-#define _syscall3(type,name,type1,arg1,type2,arg2,type3,arg3) \
-do{ \
-long __res; \
-__asm__ volatile (  "movq %1, %%rax ; movq %2, %%rbx; movq %3, %%rcx; movq %4, %%rdx; int $0x80; movq %%rax, %0;" \
-                  : "=m" (__res) \
-                  : "g" (__NR_##name),"r" ((long)(arg1)),"r" ((long)(arg2)), "r" ((long)(arg3)) \
-		  : "rax","memory","rbx","rcx","rdx"); \
-return (type) (__res); \
-}while(0)
-
 #define _syscall4(type,name,type1,arg1,type2,arg2,type3,arg3,type4,arg4)\
 do{ \
 long __res; \
