5debdfbd2fc840f08f03e6fad54474d9f1387f2b
ashrithhc <ashrithhcs@gmail.com>
2018-05-01 12:50:07 -0400

    schedule in process

--- sys/proc_man.c
+++ sys/proc_man.c
@@ -3,48 +3,16 @@
 #include <sys/mem.h>
 #include <sys/kprintf.h>
 int np = 0;
-void init_proc(){
-	for(int i=0;i<MAX;i++){
-		taskQueue[i].state = READY;
-		taskQueue[i].fd_c = 0;
-		taskQueue[i].time = 0;
-	}
-}
 
-void loadPML4E(uint64_t newPML4){
-	__asm__ __volatile__("movq %0, %%cr3;" : : "r"(newPML4) : );
-}
+
 
 void switchRSP(taskStruct *finalTask){
-	__asm__ __volatile__("movq %%rsp, %0;" : "=g"((&(finalTask->regs))->rsp) : : "memory");
-	__asm__ __volatile__("movq %0, %%rsp;" : : "r"((&(currentTask->regs))->rsp) : "memory");
+	__asm__ __volatile__("movq %%rsp, %0;" : "=g"() : : "memory");
+	__asm__ __volatile__("movq %0, %%rsp;" : : "r"() : "memory");
 }
 
-void switchRIP(taskStruct *finalTask){
-	__asm__ __volatile__ ("movq $1f, %0" : "=g"((&(finalTask->regs))->rip) : : );
-	__asm__ __volatile__ ("pushq %0;" : : "r"((&(currentTask->regs))->rip) : );
-}
 
-void schedule(){
-	taskStruct *finalTask = currentTask;
-	int nextPID = (finalTask->pid+1) % MAX;
-	while(nextPID != finalTask->pid){         
-		if(taskQueue[nextPID].state == RUNNING){
-			break;
-		}
-		nextPID = (nextPID+1)%MAX;
-	}
-    currentTask = (taskStruct *)&taskQueue[nextPID];
-	set_tss_rsp((uint64_t*)&(currentTask->kstack[511]));
-	// __asm__ __volatile__("movq %%rsp, %0;" : "=g"((&(finalTask->regs))->rsp) : : "memory");
-	// __asm__ __volatile__("movq %0, %%rsp;" : : "r"((&(currentTask->regs))->rsp) : "memory");
-	switchRSP(finalTask);
-	loadPML4E(currentTask->pml4e);
-	__asm__ __volatile__ ("movq $1f, %0" : "=g"((&(finalTask->regs))->rip) : : );
-	__asm__ __volatile__ ("pushq %0;" : : "r"((&(currentTask->regs))->rip) : );
-	__asm__ __volatile__("retq");
-	__asm__ __volatile__ ("1:\t");
-}
+
 
 
 
--- sys/process.c
+++ sys/process.c
@@ -46,6 +46,18 @@ int newPID(){
 	return -1;
 }
 
+void init_proc(){
+    for(int i=0; i<MAX; i++){
+        (&(taskQueue[i]))->state = READY;
+        (&(taskQueue[i]))->fd_c = 0;
+        (&(taskQueue[i]))->time = 0;
+    }
+}
+
+void loadPML4E(uint64_t newPML4){
+    __asm__ __volatile__("movq %0, %%cr3;" : : "r"(newPML4) : );
+}
+
 void *memcpy(void *dest, const void *src, int n){
     unsigned char *retDest = (unsigned char *)dest;
     const unsigned char *retSrc = (unsigned char *)src;
@@ -550,6 +562,26 @@ unsigned int sleep(unsigned int seconds){
     currentTask->state = HANG;
     schedule();
     return 0;
+}
+
+void schedule(){
+    taskStruct *finalTask = currentTask;
+    int nextPID = (finalTask->pid+1) % MAX;
+    while(nextPID != finalTask->pid){         
+        if(taskQueue[nextPID].state == RUNNING){
+            break;
+        }
+        nextPID = (nextPID+1)%MAX;
+    }
+    currentTask = (taskStruct *)&taskQueue[nextPID];
+    set_tss_rsp((uint64_t*)&(currentTask->kstack[511]));
+    __asm__ __volatile__("movq %%rsp, %0;" : "=g"((&(finalTask->regs))->rsp) : : "memory");
+    __asm__ __volatile__("movq %0, %%rsp;" : : "r"((&(currentTask->regs))->rsp) : "memory");
+    loadPML4E(currentTask->pml4e);
+    __asm__ __volatile__ ("movq $1f, %0" : "=g"((&(finalTask->regs))->rip) : : );
+    __asm__ __volatile__ ("pushq %0;" : : "r"((&(currentTask->regs))->rip) : );
+    __asm__ __volatile__("retq");
+    __asm__ __volatile__ ("1:\t");
 }
 
 int chdir(char* path){
