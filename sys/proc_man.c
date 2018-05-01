363184a8e0ea694c921eddf412e5745391236897
ashrithhc <ashrithhcs@gmail.com>
2018-05-01 12:50:53 -0400

    schedule in process

--- sys/proc_man.c
+++ sys/proc_man.c
@@ -6,11 +6,6 @@ int np = 0;
 
 
 
-void switchRSP(taskStruct *finalTask){
-	__asm__ __volatile__("movq %%rsp, %0;" : "=g"() : : "memory");
-	__asm__ __volatile__("movq %0, %%rsp;" : : "r"() : "memory");
-}
-
 
 
 
