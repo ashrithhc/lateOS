01028a9bf392e2199e53561504ac579e8ce0fdee
ashrithhc <ashrithhcs@gmail.com>
2018-05-01 09:05:41 -0400

    merging headers

--- include/sys/elf64.h
+++ include/sys/elf64.h
@@ -44,4 +44,37 @@ typedef struct Elf64_Phdr{
   Elf64_Xword   p_align;
 } Elf64_Phdr;
 
+extern char _binary_tarfs_start;
+extern char _binary_tarfs_end;
+
+struct posix_header_ustar {
+  char name[100];
+  char mode[8];
+  char uid[8];
+  char gid[8];
+  char size[12];
+  char mtime[12];
+  char checksum[8];
+  char typeflag[1];
+  char linkname[100];
+  char magic[6];
+  char version[2];
+  char uname[32];
+  char gname[32];
+  char devmajor[8];
+  char devminor[8];
+  char prefix[155];
+  char pad[12];
+};
+
+void init_tarfs();
+
+uint64_t get_file_address(char* filename);
+
+int isfileexists(char* path);
+
+void setTruePath(char* abs_path);
+
+int isValidDirectory(char* path);
+
 #endif
