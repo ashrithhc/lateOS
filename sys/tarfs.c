ad594a74e9b383b83527a16bb7db61c566c044e3
ashrithhc <ashrithhcs@gmail.com>
2018-05-02 06:13:31 -0400

    last try

--- sys/tarfs.c
+++ sys/tarfs.c
@@ -69,7 +69,7 @@ void setTruePath(char* absPath){
     }
     else strcpy(file_path,absPath+1);
     resetString(absPath);
-    int pathOffset = ]getOffset(absPath, 0);
+    int pathOffset = getOffset(absPath, 0);
     for(index = 0; *(file_path+index) != '\0'; index++)
     {
         if(previousDir(file_path, index))
