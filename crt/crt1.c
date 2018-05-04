a245ff14b624a68ee14513495c5cb5424b01d02f
ashrithhc <ashrithhcs@gmail.com>
2018-05-04 03:40:29 -0400

    code clean

--- crt/crt1.c
+++ crt/crt1.c
@@ -29,5 +29,22 @@ void initEnvironment(){
 }
 
 void _start(void) {
-  initEnvironment();
+  // initEnvironment();
+  globalENV = checkforENV();
+  __asm__ __volatile__ ("movq %%rsp, %0; movq %%rsp, %1" : "=m"(setENVvalue), "=m"(getENVvalue) : :"rsp");
+  getENVvalue = getENVvalue + 2;
+  for(iter = 0; iter < *(setENVvalue + 2); iter++){
+    argv[iter] = (*(getENVvalue++));  
+  }
+  globalENV++;
+  iter=0;
+  getENVvalue++;
+
+  while( (*getENVvalue) != NULL){
+    env[iter++] = (*(getENVvalue++));
+  }
+  globalENV--;
+  pushenvs(env);
+  int retVal = main( *(setENVvalue + 2), argv, env);
+  exit(retVal);
 }
