4cabcb9c14046caec0252b4b6e1f538499187d0b
ashrithhc <ashrithhcs@gmail.com>
2018-05-04 03:30:59 -0400

    code clean

--- crt/crt1.c
+++ crt/crt1.c
@@ -1,26 +1,20 @@
 #include <stdlib.h>
 
- int *adr;
- char **adr2;
- char* argv[20],*env[100];
-
 void _start(void) {
-  __asm__ __volatile__ ("movq %%rsp, %0; movq %%rsp, %1" : "=m"(adr), "=m"(adr2) : :"rsp");
-  int k=0;
-  adr2 = adr2 + 2;
-  //adr2 = adr+3;
-  for( k=0;k<*(adr+2);k++)
-  {
-	argv[k] = (*(adr2++));	
+  int *setENVvalue, iter; char **getENVvalue; char* argv[20], *env[100];
+  __asm__ __volatile__ ("movq %%rsp, %0; movq %%rsp, %1" : "=m"(setENVvalue), "=m"(getENVvalue) : :"rsp");
+  getENVvalue = getENVvalue + 2;
+  for(iter = 0; iter < *(setENVvalue + 2); iter++){
+    argv[iter] = (*(getENVvalue++));	
   }
-  k=0;
-  adr2++;
-  while((*adr2)!=NULL){
-	env[k++] = (*(adr2++));
+  iter=0;
+  getENVvalue++;
+
+  while( (*getENVvalue) != NULL){
+    env[iter++] = (*(getENVvalue++));
   }
-  //while(env[k-1]!=NULL);
   pushenvs(env);
-  int i = main(*(adr+2),argv,env);
-  // call main() and exit() here
-  exit(i);
+
+  int retVal = main( *(setENVvalue + 2), argv, env);
+  exit(retVal);
 }
