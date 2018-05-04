a867cbcb33ea6b51501d6e7c5675f9e70ace2546
ashrithhc <ashrithhcs@gmail.com>
2018-05-04 06:24:58 -0400

    code clean

--- include/sys/wait.h
+++
@@ -1,4 +0,0 @@
-#ifndef _WAIT_H
-#define _WAIT_H
-pid_t wait(int *status);
-#endif
--- include/syscall.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SYSCALL_H
-#define _SYSCALL_H
-#include "syscall_const.h"
-
- // https://www.linuxjournal.com/article/4048
-#endif
